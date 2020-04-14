8ac303f1fd37075de3f702da12c3b572fbc9e0f3
Jens Waage <waage@10.22.10.162>
2020-04-14 17:21:08 +0200

    Finished ex5 p1

---
+++ Exercise 5/part_1.py
@@ -0,0 +1,55 @@
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+import os
+import pickle
+
+def preprocess(data):
+    vectorizer = HashingVectorizer(stop_words='english', n_features=2**18, binary=True, norm=None)
+    return vectorizer.transform(data)
+
+def store_preprocess(data, filename):
+    with open(filename, 'wb') as train_data:
+        pickle.dump(data, train_data)
+
+def load_training_data():
+    y_train = pickle.load(open('Data/sklearn-data.pickle', 'rb'))['y_train']
+    if not os.path.exists('Data/train-data.pickle'):
+        x_train = preprocess(pickle.load(open('Data/sklearn-data.pickle', 'rb'))['x_train'])
+        store_preprocess(x_train, 'Data/train-data.pickle')
+    else:
+        x_train = pickle.load(open('Data/train-data.pickle', 'rb'))
+    return x_train, y_train
+
+def load_test_data():
+    y_test = pickle.load(open('Data/sklearn-data.pickle', 'rb'))['y_test']
+    if not os.path.exists('Data/test-data.pickle'):
+        x_test = preprocess(pickle.load(open('Data/sklearn-data.pickle', 'rb'))['x_test'])
+        store_preprocess(x_test, 'Data/test-data.pickle')
+    else:
+        x_test = pickle.load(open('Data/test-data.pickle', 'rb'))
+    return x_test, y_test
+
+
+
+
+
+x_train, y_train = load_training_data()
+x_test, y_test = load_test_data()
+
+# Naive Bayes classificiation
+classifier = BernoulliNB()
+
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+
+print(f"Naive Bayes accuracy: {accuracy_score(y_test, y_pred)}")
+
+# Decision Tree classifier
+classifier = DecisionTreeClassifier(min_samples_leaf=10, min_impurity_decrease=0.01)
+
+classifier.fit(x_train, y_train)
+y_pred = classifier.predict(x_test)
+
+print(f"Decision Tree accuracy: {accuracy_score(y_test, y_pred)}")
